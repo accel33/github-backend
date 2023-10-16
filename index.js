902d47b3ecfcfc2df13b8266f7120c90913badf1
accel33 <accelwtf@gmail.com>
2023-10-16 11:42:44 -0500

    update: combinacion de ambos repositorios

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 module.exports.handler = async (event) => {
   const repo = 'culqi';
+  const repo2 = 'github-backend';
   const owner = 'accel33';
   const url = 'https://api.github.com/repos/' + owner + '/' + repo + '/commits';
-  console.log(url);
+  const url2 =
+    'https://api.github.com/repos/' + owner + '/' + repo2 + '/commits';
   const response = await fetch(url);
+  const response2 = await fetch(url2);
   const jsonResponse = await response.json();
-  console.log(jsonResponse);
-  return jsonResponse;
+  const jsonResponse2 = await response2.json();
+  const finalResponse = [...jsonResponse2, ...jsonResponse];
+  console.log(finalResponse);
+  return finalResponse;
 };
